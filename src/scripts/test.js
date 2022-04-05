bdea62ad760f188f0acb5e4cee685cdf02cfef49
App Academy Student <email@email.com>
2022-04-05 10:13:52 -0400

    clear definitions

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,17 +1,50 @@
-fetch('https://api.teleport.org/api/urban_areas/')
-  .then((res) => {
-    let data1 = []
-    if (res.ok) { // ok is a method we can call on our response; returns a boolean
-      console.log("success");
-      return res.json();
-    } else {
-      throw new Error('Network response was not ok');
-    }
 
-  })
+JSONDATA = "";
+// let apiUrl = "https://api.teleport.org/api/urban_areas/" map this to names
+// thhen iterate through names and map on the additional data I need 
 
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+// https://api.teleport.org/api/urban_areas/ - names 
+// // need lattitude & lon for the globe locations, on the globe i'd like event listeners and to feed the bar chart from the click 
+// https://api.teleport.org/api/urban_areas/slug:belfast for each name i'd like the overall score and latlon and summary
+// https://api.teleport.org/api/urban_areas/slug:belfast/scores/
+
+// maybe individual scores for better bar graph later
+async function getJson(url) {
+  let response = await fetch(url);
+  let data = await response.json()
+  return data;
+}
+
+async function main() {
+  // //OPTION 1
+  // getJson(apiUrl)
+  //   .then(data => console.log(data));
+
+  //OPTION 2
+  jsondata = await getJson(apiUrl)
+  console.log(jsondata);
+}
+main();
+console.log(JSONDATA);
+
+
+
+// fetch('https://api.teleport.org/api/urban_areas/')
+//   .then((res) => {
+  //     let data1 = []
+  //     if (res.ok) {
+//       console.log("success");
+//       return res.json();
+//     } else {
+//       throw new Error('Network response was not ok');
+//     }
+
+//   })
+
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+
 // console.log(data1);
 
 // let jsondata;
@@ -23,16 +56,6 @@ fetch('https://api.teleport.org/api/urban_areas/')
 //   }
 // )
 // console.log(jsondata)
-
-
-
-// https://api.teleport.org/api/urban_areas/ - names 
-// // need lattitude & lon for the globe locations, on the globe i'd like event listeners and to feed the bar chart from the click 
-// https://api.teleport.org/api/urban_areas/slug:belfast for each name i'd like the overall score and latlon and summary
-// https://api.teleport.org/api/urban_areas/slug:belfast/scores/
-
-// maybe individual scores for better bar graph later
-
 
 
 // $.get('https://api.teleport.org/api/countries/').then(function (data) {
